51d86a3bd5f4d354ec701fe7a4c099e2474f5292
neto1895 <cruz.rosales.ernesto@gmail.com>
2023-07-03 18:04:14 -0600

    create css style for full page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -79,4 +79,23 @@ router.get('/login', (req, res) => {
   res.render('login');
 });
 
+router.get('/dashboard', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Project }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('dashboard', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
