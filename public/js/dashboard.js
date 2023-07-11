151b4d62262be8f5df906cf78e1e51cb3dab4683
neto1895 <cruz.rosales.ernesto@gmail.com>
2023-07-10 19:36:51 -0600

    full functionality of create new POST in dashboard

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -4,3 +4,30 @@
 function redirectToBlogPost(blogpostId) {
   window.location.href = `/blogpost/${blogpostId}`;
 }
+
+const newBlogpostEntry = async (event) => {
+  event.preventDefault();
+
+  const title = document.querySelector('#titleNewPost').value.trim();
+  const content = document.querySelector('#contentNewPost').value.trim();
+
+  if (title && content) {
+    const response = await fetch(`/api/blogposts`, {
+      method: 'POST',
+      body: JSON.stringify({ title, content}),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert('Failed to create new post');
+    }
+  }
+};
+
+
+document
+  .querySelector('.new-blogpost-form')
+  .addEventListener('submit', newBlogpostEntry);
