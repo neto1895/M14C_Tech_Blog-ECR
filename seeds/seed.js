c901d1efbe76d2566be12011dd3a1428a8cb9565
neto1895 <cruz.rosales.ernesto@gmail.com>
2023-07-14 01:11:11 -0600

    clean test seeds

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,11 +1,9 @@
 const sequelize = require('../config/connection');
-const { User, Project, Blogpost, Comment, PostComment } = require('../models');
+const { User, Blogpost, Comment } = require('../models');
 
 const userData = require('./userData.json');
-const projectData = require('./projectData.json');
 const blogpostData = require('./blogpostData.json');
 const commentData = require('./commentData.json');
-// const postCommentData = require('./postCommentData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -15,23 +13,8 @@ const seedDatabase = async () => {
     returning: true,
   });
 
-  for (const project of projectData) {
-    await Project.create({
-      ...project,
-      user_id: users[Math.floor(Math.random() * users.length)].id,
-    });
-  }
-  for (const blogpost of blogpostData) {
-    await Blogpost.create({
-      ...blogpost,
-      user_id: users[Math.floor(Math.random() * users.length)].id,
-    });
-  }
-
-
-
+  const blogpost = await Blogpost.bulkCreate(blogpostData);
   const comment = await Comment.bulkCreate(commentData);
-  // const postComment = await PostComment.bulkCreate(postCommentData);
 
   process.exit(0);
 };
